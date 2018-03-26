3de2ee4871941fdfb275c54536df9c632a744098
Gabriel Alves <g04lves@gmail.com>
2018-03-26 16:35:12 -0300

    Changes poison version to 3.1, refactors, removes, updates elixir, etc.

--- test/support/timeutilsmock.ex
+++ test/support/timeutilsmock.ex
@@ -1,5 +1,3 @@
 defmodule Jwt.TimeUtils.Mock do
-
-    def get_system_time(), do: Application.get_env(:jwt, :current_time_for_test)
-
+  def get_system_time(), do: Application.get_env(:jwt, :current_time_for_test)
 end
